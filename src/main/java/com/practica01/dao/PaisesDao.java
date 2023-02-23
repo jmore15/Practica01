1bd0bf6d47542a727fa2bb6df68a1fc7f7866634
jmore15 <jonmoreira15@outlook.com>
2023-02-22 20:42:08 -0600

    Practica01 Actualización Jonathan

--- src/main/java/com/practica01/dao/PaisesDao.java
+++ src/main/java/com/practica01/dao/PaisesDao.java
@@ -1,13 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package domain;
+package com.practica01.dao;
 
 /**
  *
  * @author Josseline Jiménez
  */
-public class Pais {
+public interface PaisesDao {
     
 }
